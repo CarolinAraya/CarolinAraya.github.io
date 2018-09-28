d511e76d69e881baaf468466d9aed17e5c13e7a0
DESKTOP-5H6PU6V\_caro <caro.frontend@gmail.com>
2018-09-28 12:25:26 -0300

    estructura de menú

---
+++ Menu.js
@@ -0,0 +1,12 @@
+//Menú
+
+about.me = {
+    developer =[ 
+        { "vanillaJs": ["datadashboard", "trans-lab"] },
+        {"Node": "cmd-alienks"},
+        {"Angular": "social-network"}
+    ],
+    artist = {"ilustrations": ["digitals", "watercolor"]},
+}
+
+
